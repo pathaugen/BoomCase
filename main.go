a2e66d7e03fb81b19932cff0d9df86d483ac502c
Patrick William Haugen <pathaugen@gmail.com>
2017-10-19 02:18:45 -0700

    Updated to new gcloud GAE SDK

--- main.go
+++ main.go
@@ -2,22 +2,25 @@
 package main
 
 import (
+	// oldAppengine "appengine"
+	"google.golang.org/appengine"
+
 	"fmt"
-    "net/http"
+  "net/http"
 )
 
 // ========== ========== ========== ========== ========== ========== ========== ========== ========== ==========
 // GAE config handlers for URIs
 func init() {
 	http.HandleFunc("/savecasedriver",	handlerSaveCaseDriver)
-	http.HandleFunc("/saveimage",		handlerSaveImage)
-	http.HandleFunc("/serve/",			handlerServe)
-	http.HandleFunc("/login",			handlerLogin)
-	
+	http.HandleFunc("/saveimage",				handlerSaveImage)
+	http.HandleFunc("/serve/",					handlerServe)
+	http.HandleFunc("/login",						handlerLogin)
+
 	// API Versions
-	http.HandleFunc("/api/1.0/",		handlerAPI10) // API version 1.0
-	
-    http.HandleFunc("/",				handlerRoot)
+	http.HandleFunc("/api/1.0/",				handlerAPI10) // API version 1.0
+
+  http.HandleFunc("/",								handlerRoot)
 }
 
 // if true, running in production environment
@@ -26,7 +29,9 @@ func init() {
 
 // main() used for executable when running after build, and also required for Travis CI
 func main() {
+	appengine.Main()
+
 	fmt.Println("BoomCase Google App Engine Application. Deploy code to Google App Engine (GAE) to utilize. No command line functions available.")
-	
+
 	// TODO: Creation of offline BoomCase customization web server application for POS kiosks utilizing same code
 }
